b56ef556ffc0f5312714cae8bda656e82f521d63
ZaviWayne <248329820+ZaviWayne@users.noreply.github.com>
2026-08-26 17:36:39 +0800

    初始框架

---
+++ src/main/java/com/zaviwayne/asynctask/AsyncTaskSpringBootStarterApplication.java
@@ -0,0 +1,11 @@
+package com.zaviwayne.asynctask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskSpringBootStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTaskSpringBootStarterApplication.class, args);
+    }
+}
